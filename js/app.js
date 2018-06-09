588e67d3d185b9ffea29cc49cd6d20220e210d47
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-06-08 19:38:44 -0500

    Add list of cards and add card flip functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
+ const cards = [ "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle"];
 
 
 /*
@@ -26,6 +27,7 @@ function shuffle(array) {
 }
 
 
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -34,5 +36,20 @@ function shuffle(array) {
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this    const card = document.querySelector(".card");
+ other function that you call from this one)
  */
+
+ //show card
+ const everyCard = document.querySelectorAll(".card");
+
+ everyCard.forEach(function(card) {
+   card.addEventListener("click", function(evt) {
+     card.classList.add("open", "show");
+   });
+});
+
+
+ //add open card to list of open cards
+ //check for card matches
+ //remove non-matching cards from list and hide symbol
