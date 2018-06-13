f313838b62d057ded344a6c93f0990e2013f6f4f
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-06-12 19:13:09 -0500

    Add list of functions needed for game play, including working saveCards function

--- js/app.js
+++ js/app.js
@@ -11,6 +11,15 @@
  *   - add each card's HTML to the page
  */
 
+//generate game board
+function createBoard() {
+
+}
+
+function startGame() {
+
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -27,29 +36,39 @@ function shuffle(array) {
 }
 
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this    const card = document.querySelector(".card");
- other function that you call from this one)
- */
-
- //show card
  const everyCard = document.querySelectorAll(".card");
+ let openCards = [];
+ const card = document.querySelector("li.card");
 
- everyCard.forEach(function(card) {
+//show card
+everyCard.forEach(function(card) {
    card.addEventListener("click", function(evt) {
      card.classList.add("open", "show");
+     console.log(card);
    });
 });
 
+//add matched cards to list of open cards
+function saveCards() {
+  const openCard = card.classList.contains("open", "show", "match");
+  openCards.push(openCard);
+  console.log(openCards);
+}
 
- //add open card to list of open cards
- //check for card matches
- //remove non-matching cards from list and hide symbol
+function matchCards() {
+
+
+}
+
+function countMoves() {
+
+}
+
+//quality of play
+function starRating() {
+
+}
+
+function gameOver() {
+
+}
