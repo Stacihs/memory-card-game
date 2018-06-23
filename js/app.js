da5e6a98b3c8a488cc699bc8d26b0fb0997d7996
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-06-23 10:40:23 -0500

    Add initial new logic and clean up code format

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
- * Create a list that holds all of your cards
- */
- const cards = [ "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-paper-plane-o", "fa-bomb", "fa-bolt", "fa-cube", "fa-bomb", "fa-leaf", "fa-bicycle"];
+* Create a list that holds all of your cards
+*/
+const cards = [ "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-paper-plane-o", "fa-bomb", "fa-bolt", "fa-cube", "fa-bomb", "fa-leaf", "fa-bicycle"];
 const deck = document.querySelector(".deck");
 const gameBoard = document.createDocumentFragment();
 let moves = 0;
@@ -18,9 +18,9 @@ const timer = setInterval(startTimer, 1000);
 
 // flip cards
 allCards.forEach(function(card) {
-   card.addEventListener("click", function(event) {
-     card.classList.add("open", "show");
-     saveCard(card);
+  card.addEventListener("click", function(event) {
+    card.classList.add("open", "show");
+    saveCard(card);
   });
 });
 
@@ -30,37 +30,37 @@ restart.addEventListener("click", function(event) {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 //generate game board
 function createBoard() {
-    shuffle(cards);
-    for (const card of cards) {
-      const li = document.createElement("li");
-      li.innerHTML = `<li class="card"><i class="fa ${card}"></i></li>`;
-      gameBoard.appendChild(li);
-      deck.appendChild(gameBoard);
-    };
+  shuffle(cards);
+  for (const card of cards) {
+    const li = document.createElement("li");
+    li.innerHTML = `<li class="card"><i class="fa ${card}"></i></li>`;
+    gameBoard.appendChild(li);
+    deck.appendChild(gameBoard);
+  };
 
-    return deck;
+  return deck;
 }
 
 function startTimer() {
-    let minutes = Math.floor(seconds / 60);
-    let otherSeconds = seconds % 60;
-    seconds++;
-    clock.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${otherSeconds < 10 ? '0' : ''}${otherSeconds}`;
+  let minutes = Math.floor(seconds / 60);
+  let otherSeconds = seconds % 60;
+  seconds++;
+  clock.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${otherSeconds < 10 ? '0' : ''}${otherSeconds}`;
 }
 
 function stopTimer() {
@@ -74,14 +74,14 @@ function saveCard(card) {
 
 //check if cards in openCards list match
 function matchCards() {
-// keep matched cards displayed
-  if (openCards[1].isEqualNode(openCards[0])) {
+  // keep matched cards displayed
+  if (openCards[0].isEqualNode(openCards[1])) {
     openCards[0].classList.add("match");
     openCards[1].classList.add("match");
     matchedCards.push(openCards[0], openCards[1]);
     openCards = [];
   } else {
-// flip unmatched cards back over
+    // flip unmatched cards back over
     openCards[0].classList.remove("open", "show");
     openCards[1].classList.remove("open", "show");
     openCards = [];
@@ -106,5 +106,8 @@ function gameOver() {
 
 // start a new game
 function newGame() {
-
+  moves = 0;
+  seconds = 0;
+  deck.innerHTML = "";
+  createBoard();
 }
